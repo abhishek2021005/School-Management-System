f3ca8563603d85209978511c5298dce4e003417d
Abhishek Kumar <abhishek2021005@gmail.com>
2024-06-20 00:02:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,9 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: "10mb" }));
-// app.use(cors());
+
 app.use(cors());
-// app.use(cors({
-//   // origin: 'https://school-management-system-n8h8xx5dn-abhishek2021005s-projects.vercel.app',
-//  origin: 'https://school-management-system-git-main-abhishek2021005s-projects.vercel.app/',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-// }));
+
 
 mongoose
   .connect(process.env.MONGO_URL, {
